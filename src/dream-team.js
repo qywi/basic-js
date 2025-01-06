ce5a614b67117d081f2e2d58a52d9b95b848a38b
qywi <teslagamnesla.123456789@gmail.com>
2025-01-06 14:51:24 +0200

    feat: add new functional

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let teamName = [];
+
+  for (let member of members) {
+    if (typeof member === 'string') {
+      teamName.push(member.trim()[0].toUpperCase());
+    }
+  }
+  return teamName.sort().join('');
 }
 
 module.exports = {
